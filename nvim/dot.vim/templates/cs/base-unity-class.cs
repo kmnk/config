e4d0dd7eabd721a418a8e6ce08e646a6bc0e74a6
kmnk <kmnknmk+com-github@gmail.com>
2019-01-28 11:15:17 +0900

    Update

--- nvim/dot.vim/templates/cs/base-unity-class.cs
+++ nvim/dot.vim/templates/cs/base-unity-class.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 /// <summary>クラスの大まかな説明</summary>
 /// <seealso>https://msdn.microsoft.com/ja-jp/library/b2s063f7.aspx</seealso>
 [System.Serializable]
-public class {{_expr_:expand('%:t:r')}} {
+public class {{_expr_:expand('%:t:r')}}
+{
 
 }
